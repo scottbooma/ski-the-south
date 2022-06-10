adc25f0cfbb3de77ea3e00a755d1ff5844ce409a
scottbooma <scottbooma@gmail.com>
2022-06-10 14:44:47 -0400

    Move data from template to component

--- src/app/mountain-stats/mountain-stats.component.ts
+++ src/app/mountain-stats/mountain-stats.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-mountain-stats',
   templateUrl: './mountain-stats.component.html',
-  styleUrls: ['./mountain-stats.component.css']
+  styleUrls: ['./mountain-stats.component.css'],
 })
 export class MountainStatsComponent implements OnInit {
+  location = 'Maggie Valley, NC';
+  elevation = "5,400'";
+  vertical = "740'";
+  acres = 50;
+  trails = 18;
+  lifts = 5;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
